b74e1616cf3fa6878134ead5b1509aa7b5f5019b
Shawon Ashraf <shawon13@live.com>
2018-11-16 02:29:06 +0600

    added missing loop to download songs

--- utils/downloader.js
+++ utils/downloader.js
@@ -1,10 +1,15 @@
 const http = require('http');
 const fs = require('fs');
 
-let download = (file_name, link) => {
-  console.log(`Now downloading => ${file_name} ...`);
-  let file = fs.createWriteStream(`./downloads/${file_name}`);
-  http.get(link, res => res.pipe(file));
+const downloadLinks = require('../links.json');
+
+let download = () => {
+  downloadLinks.forEach(song => {
+    const { file_name, link } = song;
+    console.log(`Now downloading => ${file_name} ...`);
+    let file = fs.createWriteStream(`./downloads/${file_name}`);
+    http.get(link, res => res.pipe(file));
+  });
 };
 
 module.exports = {
