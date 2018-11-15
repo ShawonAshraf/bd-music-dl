4699baacb3b1f90fe00708c8ca267ca6523edea3
Shawon Ashraf <shawon13@live.com>
2018-11-16 02:15:44 +0600

    added comments

--- utils/process-urls.js
+++ utils/process-urls.js
@@ -6,14 +6,18 @@ let getCorrectlyEncodedDownloadUrl = (songUrl) => {
 };
 
 
+// get the song file name from the url
 let getFileName = (songUrl) => {
   let fileName = songUrl.split(' - ')[1];
   return fileName;
 };
 
+
+// remove the html extension from  the end of the URL
 let removeHtmlExtension = (fetchedUrl) => {
   return fetchedUrl.replace('.html', '');
 };
+
 
 module.exports = {
   getCorrectlyEncodedDownloadUrl,
