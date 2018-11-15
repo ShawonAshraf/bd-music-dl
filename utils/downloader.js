2e6023892f0086a4cdb2481833d11b8e8fe7d984
Shawon Ashraf <shawon13@live.com>
2018-11-16 02:24:23 +0600

    downloader shows file info

--- utils/downloader.js
+++ utils/downloader.js
@@ -2,6 +2,7 @@ const http = require('http');
 const fs = require('fs');
 
 let download = (file_name, link) => {
+  console.log(`Now downloading => ${file_name} ...`);
   let file = fs.createWriteStream(`./downloads/${file_name}`);
   http.get(link, res => res.pipe(file));
 };
