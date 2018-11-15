009715bfa5824167c62f74c473f8bdf3c9aa535a
Shawon Ashraf <shawon13@live.com>
2018-11-16 03:05:48 +0600

    removed redundant console log

--- utils/save-links-json.js
+++ utils/save-links-json.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 
 let saveToJSONFile = (payload) => {
-  console.log('writing to json');
   const jsonFilePath = './links.json';
   let jsonString = JSON.stringify(payload);
 
