357e9858079129d94dad114468a2370d68b71c70
Shawon Ashraf <shawon13@live.com>
2018-11-16 05:08:46 +0600

    renamed doTheScrapping

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const argv = yargs.argv;
 const downloadPath = argv.path;
 const url = argv.link;
 
-let doTheScrapping = async () => {
+let scrap = async () => {
   console.log('Preparing the downloader ...');
   await scrapper.scrap(url);
 };
 
-doTheScrapping().then(() => {
+scrap().then(() => {
   console.log('Downloading ... grab a cup of coffee or something chilled while this works!');
   downloader.download(downloadPath);
 }).catch((e) => {
