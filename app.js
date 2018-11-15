d4c03acf06d79855c0f93a0d9253b698c0535574
Shawon Ashraf <shawon13@live.com>
2018-11-16 03:05:01 +0600

    downloading works

--- app.js
+++ app.js
@@ -8,9 +8,14 @@ const downloadPath = argv.path;
 const url = argv.link;
 
 let doTheScrapping = async () => {
+  console.log('Preparing the downloader ...');
   await scrapper.scrap(url);
 };
 
 doTheScrapping().then(() => {
+  console.log('Downloading ... grab a cup of coffee or something chilled while this works!');
   downloader.download(downloadPath);
+}).catch((e) => {
+  console.log(e.toString());
 });
+
